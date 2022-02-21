b224018eb44844f4953b0a74646f5b31a81bf2fc
99050823 <99050823@mydavinci.nl>
2022-02-21 14:31:40 +0100

    Commit - Stemwijzer
    - Added button controll

--- Stemwijzer - Project/script/script.js
+++ Stemwijzer - Project/script/script.js
@@ -9,10 +9,20 @@ class Question {
 const textEl = document.getElementById("text");
 const countEl = document.getElementById("count");
 const titleEl = document.querySelector("h3");
+const buttonPanel = document.querySelector(".button-panel");
 
-var num = 1;
-var startBttn = document.querySelector("button");
-startBttn.onclick = initialize;
+var num = 0;
+var eensScore = 0;
+var oneensScore = 0;
+
+var buttons = document.querySelectorAll("button");
+buttons[4].onclick = initialize;
+
+for (let i = 0; i < 3; i++) {
+    buttons[i].addEventListener("click", () => {
+        bttnControll(i);
+    })
+}
 
 function generateQuestions (count) {
     return subjects[count].statement;
@@ -23,6 +33,13 @@ function generateTitle (count) {
 }
 
 function initialize () {
+    buttons[3].style.display = "inline";
+    buttonPanel.style.display = "block";
+
+    for (let i = 0; i < 3; i++) {
+        buttons[i].style.backgroundColor = null;
+        buttons[i].style.color = "black";
+    }
 
     if (num == subjects.length) {
         endFunc();
@@ -32,8 +49,8 @@ function initialize () {
         countEl.innerHTML = "Vraag " + question.count;
         titleEl.innerHTML = question.title;
         textEl.innerHTML = question.text;
-        startBttn.innerHTML = "Next";
-    
+
+        buttons[4].innerHTML = "Next";
         num++;
     }   
 }
@@ -42,7 +59,37 @@ function endFunc () {
     countEl.innerHTML = " ";
     titleEl.innerHTML = "END";
     textEl.innerHTML = " ";
-    startBttn.innerHTML = "END";
+    buttons[4].innerHTML = "Refresh";
 
-    num = 1;
+    buttons[4].onclick = () => {
+        window.location.reload();
+    }
+}
+
+function bttnControll (num) {
+    let scoreBool = [false, false, false];
+
+    buttons[0].style.color = "white";
+    buttons[1].style.color = "white";
+    buttons[2].style.color = "white";
+
+    if (num == 0) {
+        buttons[0].style.backgroundColor = "blue";
+        buttons[1].style.backgroundColor = "red";
+        buttons[2].style.backgroundColor = "red";
+
+        scoreBool[num] = scoreBool[num] ? false : true;
+    } else if (num == 1) {
+        buttons[0].style.backgroundColor = "red";
+        buttons[1].style.backgroundColor = "blue";
+        buttons[2].style.backgroundColor = "red";
+
+        scoreBool[num] = scoreBool[num] ? false : true;
+    } else if (num == 2) {
+        buttons[0].style.backgroundColor = "red";
+        buttons[1].style.backgroundColor = "red";
+        buttons[2].style.backgroundColor = "blue";
+
+        scoreBool[num] = scoreBool[num] ? false : true;
+    }
 }
