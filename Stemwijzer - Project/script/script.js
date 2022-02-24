58f5c8a6263038edd36fbbed52b7bb6109ba747c
99050823 <99050823@mydavinci.nl>
2022-02-24 15:08:13 +0100

    Commit - Stemwijzer
    - Added basic score system
    - Added background image

--- Stemwijzer - Project/script/script.js
+++ Stemwijzer - Project/script/script.js
@@ -16,6 +16,7 @@ const terug = document.getElementById("terugBttn");
 var num = 0;
 var eensScore = 0;
 var oneensScore = 0;
+var checkBool = [false, false, false];
 
 var buttons = document.querySelectorAll("button");
 start.addEventListener("click", () => {
@@ -39,29 +40,31 @@ function initialize () {
 
     for (let i = 0; i < 3; i++) {
         buttons[i].style.backgroundColor = null;
-        buttons[i].style.color = "black";
     }
+
+    scoreCheck();
 
     if (num == subjects.length) {
         endFunc();
     } else {
-
         getButtons();
         let question = new Question(generateTitle(num), generateQuestions(num));
 
         countEl.innerHTML = "Vraag " + count;
         titleEl.innerHTML = question.title;
         textEl.innerHTML = question.text;
-
         start.innerHTML = "Next";
+        
         num++;
     }   
+    checkBool = [false, false, false];
 }
 
 function endFunc () {
     countEl.innerHTML = " ";
-    titleEl.innerHTML = "END";
-    textEl.innerHTML = "Eens aantal = <br>Oneens aantal = "; 
+    titleEl.innerHTML = finalResult();
+    textEl.innerHTML = "Eens Score: " + eensScore  + "<br>" + 
+    "Oneens Score: " + oneensScore; 
     start.innerHTML = "Refresh";
 
     buttonPanel.style.display = "none";
@@ -72,23 +75,42 @@ function endFunc () {
     })
 }
 
-function bttnControll () {
+function getButtons () {
+    for (let i = 0; i < 3; i++) {
+        buttons[i].addEventListener("click", bttnControll);
+    }
+}
 
+function bttnControll () {
     var el = document.getElementById(this.id).parentElement;
     var buttonArr = el.getElementsByTagName("button");
 
     for (let i = 0; i < buttonArr.length; i++) {
         if (buttonArr[i].id == this.id) {
             buttonArr[i].style.backgroundColor = "blue";
+
+            if (this.id == "oneensBttn") {
+                checkBool[0] = checkBool[0] ? false : true;
+            } else if (this.id == "eensBttn") {
+                checkBool[2] = checkBool[2] ? false : true;
+            } else {
+                checkBool[1] = checkBool[1] ? false : true;
+            }
         } else {
             buttonArr[i].style.backgroundColor = "red";
         }
     }
 }
 
-function getButtons () {
-
-    for (let i = 0; i < 3; i++) {
-        buttons[i].addEventListener("click", bttnControll);
+function scoreCheck () {
+    if (checkBool[0] == true) {
+        oneensScore++;
+    } else if (checkBool[2] == true) {
+        eensScore++;
     }
 }
+
+function finalResult () {
+    return "(Voorbeeld: VVD)";
+}
+
